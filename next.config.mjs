b0a1da520690e1e15dfa1fff446882d006094b59
Gabe <gabrielarce3@gmail.com>
2025-01-20 01:22:10 -0800

    add search feature

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     ppr: 'incremental',
+//   },
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+  },
+};
+
+export default nextConfig;
